0bf18cc55eaaad50083fb438cfd871e11715a0ad
Alexander Brett <Alexander Brett>
2016-05-29 10:54:54 +0100

    add more viz tweaks

--- data/index.js
+++ data/index.js
@@ -1,6 +1,50 @@
-var d3 = require('d3');
 var fs = require('fs');
+var parse = require('csv-parse');
+var transform = require('stream-transform');
+console.log('start');
 
-d3.csv('ward_data.csv', function(data){
-  fs.writefile('output', {foo: 'bar'}.toString());
+
+function fix(d){
+  return typeof(d) === 'string' ? +(d.replace(',','').replace('£','')) : d;
+}
+
+var parser = parse();
+var input = fs.createReadStream('../ward_data.txt');
+var header, min, max;
+var transformation = transform(function(r, c){
+  if (!header) {
+    header = r;
+  } else if (!min){
+    min = r.slice();
+    max = r.slice();
+  } else {
+    for (var i in r){
+      min[i] = Math.min(fix(min[i]), fix(r[i]));
+      max[i] = Math.max(fix(max[i]), fix(r[i]));
+    }
+  }
+  console.log('transform')
+  return true;
 })
+
+
+transformation.on('readable', function(){
+  while(row = transformer.read()){
+    output.push(row);
+  }
+});
+
+
+input.on('end', function(d){
+  var result = {};
+  for (var i in header){
+    result[header[i]] = {min: min[i], max: max[i]};
+  }
+
+  var output = fs.writeFile('output.json', JSON.stringify(result));
+  console.log(result);
+})
+
+input.pipe(parser).pipe(transformation).pipe(process.stdout);
+
+console.log('done');
--- vis.js
+++ vis.js
@@ -1,7 +1,7 @@
 d3.select('body').append('span').text('hi!');
 
-var width = 1000;
-var height = 1000;
+var width = 1200;
+var height = 1200;
 
 function group(data){
   var result = {};
@@ -25,10 +25,14 @@ function position(alpha){
     var desired = datum.desiredLocation;
     var x = desired[0] - datum.x;
     var y = desired[1] - datum.y;
-    var force = 0.01*Math.sqrt(x*x + y*y);
+    var force = 0.005*Math.sqrt(x*x + y*y);
     datum.x += alpha*x*force;
     datum.y += alpha*y*force;
   }
+}
+
+function fix(d){
+  return typeof(d) === 'string' ? +(d.replace(',','').replace('£','')) : d;
 }
 
 d3.csv('ward_data.txt', function(data){
@@ -36,33 +40,44 @@ d3.csv('ward_data.txt', function(data){
   d3.csv('London_postcode-ONS-postcode-Directory-May15.csv', function(locationData){
 
     var wardlocations = group(locationData);
-
-    var percentToRadiusScale = d3.scale.pow().exponent(0.2);
   
     var min = 100;
     var max = 0;
 
-    var projection = d3.geo.mercator().scale(35000).center([0, 51.5085300]).translate([width/2, height/2]);
+    var projection = d3.geo.mercator().scale(70000).center([0, 51.5085300]).translate([width/2, height/2]);
+    
+    var radiusScale = d3.scale.linear()
+      .domain([
+        Math.sqrt(dataBounds.Population.min),
+        Math.sqrt(dataBounds.Population.max)])
+      .range([5,20]);
 
+    var boroughs = [];
 
     for (var i in data){
-      code = data[i].Old_Code;
+      var code = data[i].Old_Code;
       data[i].lat = wardlocations[code].lat;
       data[i].long = wardlocations[code].long;
       data[i].desiredLocation = projection([data[i].long, data[i].lat]);
-      data[i].radius = Math.sqrt(+data[i].Area)*4;
+      data[i].radius =  radiusScale(Math.sqrt(fix(data[i].Population)));//Math.sqrt(+data[i].Area)*4;
       data[i].x = data[i].desiredLocation[0];
       data[i].y = data[i].desiredLocation[1];
-      min = Math.min(min, data[i].Social_house_percent);
-      max = Math.max(max, data[i].Social_house_percent);
+      data[i].borough = data[i].Name.split('-')[0];
+      if (boroughs.indexOf(data[i].borough) < 0) boroughs.push(data[i].borough); 
     };
+
+
+
+    
       var percentToByteScale = d3.scale.linear().range([0,255]).domain([min,max]);
 
 
+  var palette = d3.scale.category20b();
+
     var force = d3.layout.force()
-        .gravity(0.0)
+        .gravity(0.01)
         .friction(0.1)
-        .charge(0)
+        .charge(function(d){return -3*d.radius})
         .nodes(data)
         .size([width, height]);
 
@@ -81,8 +96,8 @@ d3.csv('ward_data.txt', function(data){
             })
         .style("fill", function(d, i) {
            var n = Math.round(percentToByteScale(d.Social_house_percent));
-            return d3.rgb(0,n,n);
-             });
+            return palette(boroughs.indexOf(d.borough)%20)//d3.rgb(0,n,n);
+             }).append("title").text(function(d){return d.Name});
 
     force.on("tick", function(e) {
           node.each(position(e.alpha));
@@ -102,28 +117,21 @@ d3.csv('ward_data.txt', function(data){
             var y = node1.y - node2.y;
             var distance = Math.sqrt(x * x + y * y);
 
-            var minDistance = node1.radius + node2.radius + 2;
+            var minDistance = node1.radius + node2.radius + 4;
 
 
             var force =distance < minDistance ?
-              0.5*(minDistance - distance) :
-              Math.min(1, 10 / Math.pow(minDistance - distance, 3));
+             0.2*(minDistance - distance):
+              0;
             if(force > 0.1) {
               node1.x = node1.x + force*x/Math.abs(x);
               node1.y = node1.y + force*y/Math.abs(y);
               node2.x = node2.x - force*x/Math.abs(x);
               node2.y = node2.y - force*y/Math.abs(y);
-              //node1.px = node1.px + force*x/Math.abs(x);
-              //node1.py = node1.py + force*y/Math.abs(y);
-              //node2.px = node2.px - force*x/Math.abs(x);
-              //node2.py = node2.py - force*y/Math.abs(y);
             }
           }
         });
       };
     }
-
-
-    //console.log(data);
   })
 })
