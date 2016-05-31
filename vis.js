811ecb0d46af20f7f7608748c42db935d14ce0bd
Alexander Brett <alex@alexander-brett.co.uk>
2016-05-31 08:15:48 +0100

    more data

--- vis.js
+++ vis.js
@@ -138,11 +138,11 @@ function makeOptions(database){
       //"Population - 2015": radiusFromPropertyLinear('Population', 'Population', 3.2),
       //"Number of jobs in area - 2013": radiusFromPropertySqrt('Jobs', 'Jobs', 7),
       //"In employment (16-64) - 2011": radiusFromPropertyLinear('Employed', 'Employed', 3.2),
-      //"Median House Price (£) - 2014": radiusFromPropertySqrt('Median house price', 'Median_house_price', 3.5),
       "Area (sq. km)": radiusFromProperty("area", "area", "sq. km", 6.2),
       "Mayoral election 2016 total turnout size": radiusFromProperty("voting", "turnout", " voters", 2.5),
       "Mayoral election 2016 registered voters": radiusFromProperty("voting", "electorate", " voters", 2.2),
       "Population estimates & projections": radiusWithYears("population", "population", 3.2, database),
+      "Median House Price (£)": radiusWithYears('median_house_price', 'price', 3.5, database),
     };
 
  makeOptionsRadioButtons(d3.select('#sizeInput').selectAll("input").data(Object.keys(radiusMap)), radiusMap, 'sizeOption', 'radius year');
@@ -187,6 +187,45 @@ function makeOptions(database){
     }
   }
 
+   function colourWithYears(table, column, size, db){
+    var query = db.exec("SELECT DISTINCT year FROM "+table+" ORDER BY year DESC");
+    var availableYears =  query[0].values.map(function(d){ return d[0]});
+    function doMakeRadius(){
+       var query = db.exec(
+        "SELECT max("+column+"), min("+column+") FROM "+table+";"
+        + "SELECT "+column+" FROM "+table+" WHERE year="+doMakeRadius.year+" ORDER BY id ASC;"
+      );
+      var values = query[1].values.map(function(d){return d[0]});
+      var min = query[0].values[0][1];
+      var max = query[0].values[0][0];
+      var convert = d3.scale.sqrt().domain([fix(min),fix(max)]).range([230,20]);
+      function makeColour (value){
+        var n = convert(fix(value));
+        return d3.rgb(n,255-n, n);
+      }
+      this.caption = function(i){return values[i]};
+      this.colour = function(i){
+        return makeColour(values[i])
+      };
+      this.key = [{
+        caption: min,
+        colour: makeColour(min)
+      },{
+        caption: max,
+        colour: makeColour(max)
+      }];
+      return this;
+    }
+
+    doMakeRadius.years = availableYears.map(function(d){
+      return {value:d, action: function(){
+        doMakeRadius.year = d
+        }}
+    });
+    doMakeRadius.year = availableYears.filter(function(y){return y<=2016})[0];
+    return doMakeRadius;
+  }
+
   function colourFromGenericPercent(table, column){
     return function(db){
       var query = db.exec(
@@ -196,7 +235,7 @@ function makeOptions(database){
       var values = query[1].values.map(function(d){return d[0]});
       var min = query[0].values[0][1];
       var max = query[0].values[0][0];
-      var convert = d3.scale.linear().domain([fix(min),fix(max)]).range([20,255]);
+      var convert = d3.scale.linear().domain([fix(min),fix(max)]).range([230,20]);
       function makeColour (value){
         var n = convert(fix(value));
         return d3.rgb(0,n, n);
@@ -224,8 +263,15 @@ function makeOptions(database){
       //'Claimant rate of key out-of-work benefits (working age client group) (2014)': colourFromGenericPercent('OOW Benefits Recipients', 'Out_of_work_benefints_percent'),
       //"Turnout at Mayoral election - 2012":colourFromGenericPercent('Turnout', 'Mayoral_turnout'),
       "Borough": colourFromBorough(),
+      "Proportion of social rented housing": colourWithYears(
+        'housing',
+        'social*100.0/(social + owned + mortgage + private_rent + other)',
+        4,
+        database
+      ),
       "Mayoral election 2016 result": colourFromMayor(),
       "Mayoral election 2016 percentage turnout": colourFromGenericPercent('voting', 'percent_turnout'),
+      "Population density (with projections)": colourWithYears('population_density', 'density', 4, database)
     };
 
   this.makeColour = function(db){
@@ -233,7 +279,7 @@ function makeOptions(database){
   }
 
   makeOptionsRadioButtons(d3.select('#colourInput').selectAll("input")
-    .data(Object.keys(colourMap)), colourMap, 'colourOption', 'colour');
+    .data(Object.keys(colourMap)), colourMap, 'colourOption', 'colour year');
   return this;
 }
 
@@ -353,7 +399,7 @@ function startForce(data, db){
       force.resume();
     });
 
-    d3.selectAll('input[name=colourOption]').on("change", function(){
+    d3.selectAll('input[name=colourOption],input.colour.year').on("change.main", function(){
       var colourGenerator = options.makeColour(db);      
       drawColourKey(svg, colourGenerator);
       doColours(colourGenerator, nodes.transition().duration(700));
